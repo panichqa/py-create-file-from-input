668dab61907bacfc273a1850368ee1846306f509
Vlada <vlada.belayy@mail.com>
2024-08-05 19:53:01 +0300

    first

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ") + ".txt"
+    content_lines = []
+
+    while True:
+        user_input = input("Enter new line of content: ")
+        if user_input.lower() == "stop":
+            break
+        content_lines.append(user_input)
+
+    with open(file_name, "w") as file:
+        for line in content_lines:
+            file.write(line + "\n")
+
+    print(f"File {file_name} created successfully with the provided content.")
 
 
 if __name__ == "__main__":
